8bdb763e05e468e930f5a18cc4aa4cfe94449a56
Roberto Rossa <rcrossa@hotmail.com>
2022-03-08 22:56:23 -0300

    Inicial Commit.

---
+++ src/main/java/com/alkemy/preaceleracion/preaceleracion/PreaceleracionApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.preaceleracion.preaceleracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreaceleracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreaceleracionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/preaceleracion/preaceleracion/PreaceleracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.preaceleracion.preaceleracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreaceleracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
